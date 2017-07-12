e4f24c096575cec1fc2175962b704f2b19e3cf49
Y Paul Sussman <ypaulsussman@gmail.com>
2017-07-11 19:12:29 -0500

    adds tooltip, zoom, initial styling, and README

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
+### TAP Scatter Plot:
 
-### TAP Scatter Plot Plan:
+Y Paul Sussman: July 11th, 2017; Version 1.0
 
-x-axis: # recipients
+## Visualization Overview
+This graph displays the disbursement of ~9,000 grants via the [New York State Tuition Assistance Program](https://www.hesc.ny.gov/pay-for-college/apply-for-financial-aid/nys-tap.html), over the years 2000-2015. The x-axis records the number of student recipients in a given disbursement, while the y-axis records the monetary value.
 
-y-axis: $ awarded
+Each disbursement is color-coded by the university's public/private status, and mouseover reveals the disbursement's university, year, specific student count, and specific monetary value. Finally, the table is both zoom- and pan-responsive (_to better enable examination of the clustered datapoints._)
 
-bubble color: pink @ private; blue @ public
 
-click: tooltip @  
-  * university name
-  * year
-  * #/students
-  * $ awarded
+## Getting Started
+
+Fork, clone, or download the project.
+
+Download the Tuition Assistance Program `.csv` file from [data.gov](https://catalog.data.gov/dataset/tuition-assistance-program-tap-recipients-dollars-by-college-sector-group-and-level-of-stu), and add it to the directory.
+
+Strip all `$` characters from the `.csv` file.
+
+Download [Python 2.7.x](https://www.python.org/downloads/release/python-2713/), if you don't have it preinstalled. (_You can check by running_ `python -V` _in your command line._)
+
+Inside this directory, run `python -m SimpleHTTPServer` from the command line.
+
+Navigate to `localhost:8000` in your browser.
+
+## Built With
+
+* HTML5, CSS3, JavaScript (ES5), and D3.js.
+
+## Learning Value
+There were two major goals I wanted to achieve with this project. The first was to _build_ something with D3.js, rather than continuing to read through examples -- to take a dataset I was unfamiliar with, and to visually render it in an approachable way. I'm happy with those results, though some questions (_how to inject regular expressions into_ `d3.tickFormat()`_, for example, or how to create zoom-responsive circle radii_) will require further research.
+
+The second was to make something outside of the greenfield format I'd become used to. The life of a junior developer is one of frequent work on mature codebases, and so I'd wanted to practice reading and adapting code that was both preexisting and largely uncommented. Here, too, I had a fun experience -- though I imagine the process will be different when I try it with an actual full-stack application.
--- scatter_app_v1.js
+++ scatter_app_v1.js
@@ -1,12 +1,12 @@
 // Globals
-var graphWidth = screen.availWidth;
-var graphHeight = screen.availHeight;
-var radius = 3;
+var graphWidth = screen.availWidth * 0.8;
+var graphHeight = screen.availHeight * 0.8;
+var radius = 2;
 var margins = {
   left: 100,
-  top: 100,
+  top: 50,
   right: 100,
-  bottom: 100
+  bottom: 50
 };
 var axisPadding = 3;
 var totalHeight = graphHeight + margins.top + margins.bottom;
@@ -20,18 +20,20 @@ d3.csv(url, function(data) {
 
   var moneyAndStudents = data.map(function(d) {
     return {
-      yForMoney: +d["TAP Recipient Headcount"],
-      xForStudents: +d["TAP Recipient Dollars"],
-      sector: d["Sector Type"]
+      students: +d["TAP Recipient Headcount"],
+      money: +d["TAP Recipient Dollars"],
+      sector: d["Sector Type"],
+      name: d["TAP College Name"],
+      year: d["Academic Year"],
     };
   });
 
   var xExtents = d3.extent(moneyAndStudents, function(d) {
-    return d.yForMoney;
+    return d.students;
   });
 
   var yExtents = d3.extent(moneyAndStudents, function(d) {
-    return d.xForStudents;
+    return d.money;
   });
 
   var xScale = d3.scale.linear()
@@ -42,10 +44,20 @@ d3.csv(url, function(data) {
     .domain(yExtents)
     .range([0, graphHeight]);
 
-  var svg = d3.select('body')
+  var zoomBehavior = d3.behavior.zoom()
+    .scaleExtent([0.1, 100])
+    .on('zoom', onZoom);
+
+  var svg = d3.select('main')
     .append('svg')
     .attr('width', totalWidth)
-    .attr('height', totalHeight);
+    .attr('height', totalHeight)
+    .call(zoomBehavior)
+    .append('g');
+
+  var div = d3.select("main").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
   var yAxisScale = d3.scale.linear()
     .domain([yExtents[1], 0])
@@ -68,19 +80,23 @@ d3.csv(url, function(data) {
     .call(xGridlinesAxis
       // the line below caused you literally 90+ min of trouble; for whatever
       // reason, it was interacting strangely with '- graphHeight-axisPadding'
-      // as the first param. 
+      // as the first param.
       .tickSize(-graphHeight, 0, 0)
       .tickFormat(""));
   styleGridlineNodes(xGridlineNodes);
 
   var yAxisNodes = svg.append('g')
     .attr('transform', 'translate(' + (margins.left - axisPadding) + ',' + margins.top + ')')
-    .call(yAxis);
+    // note the call to d3.format *within* .tickformat()
+    .call(yAxis.tickFormat(d3.format("$,")));
   styleAxisNodes(yAxisNodes);
 
   var xAxisNodes = svg.append('g')
     .attr('transform', 'translate(' + margins.left + ',' + (totalHeight - margins.bottom + axisPadding) + ')')
-    .call(xAxis);
+    .call(xAxis.tickFormat(function(d) {
+      // regular expression below doesn't work - must find workaround
+      return addComma(d) + "\r Students";
+    }));
   styleAxisNodes(xAxisNodes);
 
   var graphGroup = svg.append('g')
@@ -92,10 +108,10 @@ d3.csv(url, function(data) {
     .append('circle')
     .attr({
       cx: function(d) {
-        return xScale(d.yForMoney);
+        return xScale(d.students);
       },
       cy: function(d) {
-        return (graphHeight - yScale(d.xForStudents));
+        return (graphHeight - yScale(d.money));
       },
       r: radius,
       fill: function(d) {
@@ -105,8 +121,32 @@ d3.csv(url, function(data) {
           return "salmon";
         }
       }
+    })
+    .on('mouseenter', function(d) {
+      div.transition()
+        .duration(200)
+        .style("opacity", 0.9);
+      div.html("<p>" + d.name + "</p><p>Year: " +
+          d.year + "</p><p>" +
+          d.students.toLocaleString('en-US') + " students</p><p>" +
+          d.money.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+          }) + "</p>")
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d) {
+      div.transition()
+        .duration(500)
+        .style("opacity", 0);
     });
 
+    function onZoom() {
+      console.log('sup');
+      svg.attr('transform', 'translate(' + d3.event.translate + ')' +
+        'scale(' + d3.event.scale + ')');
+    }
 
 }); //end d3.csv
 
@@ -139,3 +179,5 @@ function styleGridlineNodes(axisNodes) {
       stroke: 'lightgray',
     });
 }
+
+var addComma = d3.format(",");
